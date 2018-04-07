2f2d8317b7c8cb826c7f8cda3bc773a58fb45758
Hongliang Wang <hongliang7876@gmail.com>
2018-04-07 12:04:25 -0400

    update to deploy to heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+
+//Install express server
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 8080);
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default Heroku port
+app.listen(app.get('port'), function(err) {
+  if(err) console.log(err);
+  console.log('Listening on port' + app.get('port'));
+});
